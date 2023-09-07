d5a86dd5f2968e31e1928c4cbd5aefc583c18d5a
Divyanshi <divyanshi.narang16@gmail.com>
2023-09-07 16:20:51 +0530

    pizza-component

---
+++ pizza-menu/src/index.js
@@ -0,0 +1,83 @@
+//this file needs to be called index.js because module bundler(webpack)
+//in project expects the entry point to be called index.js
+
+import React from "react";
+// import ReactDOM from "react-dom";  //React v17
+import ReactDOM from "react-dom/client";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1> <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
+// Rendering the DOM in React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // StrictMode: during development render the components twice in order to find bugs or to check if we are using outdated versions of React api
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Before React v18
+// ReactDOM.render(<App />, document.getElementById("root"));
