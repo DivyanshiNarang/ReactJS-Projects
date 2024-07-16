0cdc085a37b9501815f7554bd73e23e74665e281
DivyanshiNarang <divyanshi.narang16@gmail.com>
2024-07-16 10:29:21 +0530

    world-wide start

---
+++ world-wide/src/components/AppNav/AppNav.jsx
@@ -0,0 +1,12 @@
+
+// import styles from '../AppNav/AppNav.module.css';
+
+const AppNav = () => {
+  return (
+    <div>
+      App Navigation
+    </div>
+  )
+}
+
+export default AppNav
---
+++ world-wide/src/pages/AppLayout/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '../../components/AppNav/AppNav';
+
+const AppLayout = () => {
+  return (
+    <div>
+      <AppNav />
+      <h1>Layout</h1>
+    </div>
+  )
+}
+
+export default AppLayout
