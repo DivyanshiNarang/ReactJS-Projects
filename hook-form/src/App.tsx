119fcc02490176fc00595e5ba9d52c246573cfaf
DivyanshiNarang <divyanshi.narang16@gmail.com>
2024-11-06 01:06:08 +0530

    react-hook-form

---
+++ hook-form/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HookForm from './components/HookForm'
+// import SimpleForm from './components/SimpleForm'
+
+function App() {
+
+  return (
+    <div>
+      {/* <SimpleForm /> */}
+      <HookForm />
+    </div>
+  )
+}
+
+export default App
